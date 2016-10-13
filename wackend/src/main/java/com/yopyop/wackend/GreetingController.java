d92be494bd97bf4ce6a9cbe0720f4c86cf41acbc
Sekine COULIBALY <sekine.coulibaly@drust.io>
2016-10-13 14:56:05 +0200

    Added logback dependency and trying RequestMapping

--- wackend/src/main/java/com/yopyop/wackend/GreetingController.java
+++ wackend/src/main/java/com/yopyop/wackend/GreetingController.java
@@ -2,8 +2,10 @@ package com.yopyop.wackend;
 
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 public class GreetingController {
@@ -15,5 +17,12 @@ public class GreetingController {
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
+    }
+    
+    
+    @RequestMapping(value = "/greeting/{name}", method = RequestMethod.GET, produces = "application/json")
+    public Greeting greetingUser(@PathVariable String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
     }
 }
